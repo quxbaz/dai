53dde1202b555828b6696a7131f2e104451dc372
David Yeung <quxbaz@gmail.com>
2018-07-13 16:39:35 -0600

    render

--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 
 int init_sdl ();
 SDL_Window* create_window ();
+SDL_Renderer* create_renderer ();
 int init_game ();
-void run_game_loop ();
+void run_game_loop (SDL_Renderer* renderer);
 void read_input ();
 void handle_input ();
 void quit_sdl ();
@@ -25,7 +26,7 @@ SDL_Window* create_window (int w, int h) {
   /*
     Returns 0 for success, else returns error code > 0.
   */
-  SDL_Window* window = SDL_CreateWindow("working title", 0, 0, w, h, SDL_WINDOW_SHOWN);
+  SDL_Window* window = SDL_CreateWindow("dai", 0, 0, w, h, SDL_WINDOW_SHOWN);
   if (window == NULL) {
     printf("%s", SDL_GetError());
     return NULL;
@@ -33,17 +34,23 @@ SDL_Window* create_window (int w, int h) {
   return window;
 }
 
+SDL_Renderer* create_renderer (SDL_Window* window) {
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (window == NULL) {
+    printf("%s", SDL_GetError());
+    return NULL;
+  }
+  return renderer;
+}
+
 int init_game () {
   return 0;
 }
 
 
-void run_game_loop () {
+void run_game_loop (SDL_Renderer* renderer) {
   bool should_run_game = true;
   SDL_Event event;
-
-  // Render background
-  /* SDL_ */
 
   while (should_run_game) {
     while (SDL_PollEvent(&event)) {
@@ -58,6 +65,11 @@ void run_game_loop () {
         }
       }
     }
+    // Render view
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
     SDL_Delay(16);
   }
 }
@@ -77,18 +89,24 @@ int main (int argc, char* args[]) {
     Return 0 for success, else return error code > 0.
   */
 
-  // Variable declarations
-  SDL_Window* window = NULL;
+  // Constants
   const int WINDOW_WIDTH = 640;
   const int WINDOW_HEIGHT = 480;
+
+  // Variable declarations
+  SDL_Window* window = NULL;
+  SDL_Renderer* renderer = NULL;
 
   init_sdl();
 
   window = create_window(WINDOW_WIDTH, WINDOW_HEIGHT);
   if (window == NULL) { return 1; }
 
+  renderer = create_renderer(window);
+  if (renderer == NULL) { return 1; }
+
   init_game();
-  run_game_loop();
+  run_game_loop(renderer);
 
   quit_sdl(window);
   return 0;
